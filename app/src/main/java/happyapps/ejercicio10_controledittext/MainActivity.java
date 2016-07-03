8d14ac41718af21f3c471bfcd4bb42fef811ecbf
Longynus <hacpin@hotmail.com>
2016-07-02 19:30:48 -0500

    Probado y funcionando

---
+++ app/src/main/java/happyapps/ejercicio10_controledittext/MainActivity.java
@@ -0,0 +1,33 @@
+package happyapps.ejercicio10_controledittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+
+    }
+
+
+    public void verificar(View view){
+        String clave=et2.getText().toString();
+        if(clave.length()==0){
+            Toast notification= Toast.makeText(this,"La contraseña no puede quedar vacia",Toast.LENGTH_LONG);
+            notification.show();
+        }
+    }
+
+}
